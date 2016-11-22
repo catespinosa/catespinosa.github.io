a0cfb22010030153576e52d842333be7d54a1985
Espinosa <mzy36638@admins-MacBook-Pro.local>
2016-11-21 21:29:37 -0500

    used url to get multiple track names

--- jsd920_class_repo/projects/final_project/public/js/new_app_1.js
+++ jsd920_class_repo/projects/final_project/public/js/new_app_1.js
@@ -8,31 +8,35 @@ var spotifyApi = new SpotifyWebApi();
 //   }, function(err) {
 //     console.error(err);
 //   });
-searchTrack('Llego la musica cubana')
+// searchTrack('Llego la musica cubana')
 
-function searchTrack(queryTerm) {
-  // store the current promise in case we need to abort it
-  return spotifyApi.searchTracks(queryTerm, {limit: 5})
-    .then(function(data) {
-   console.log('data', data);
-
-
-    }, function(err) {
-      console.error('error',err);
-    });
-}
+// function searchTrack(queryTerm) {
+//   // store the current promise in case we need to abort it
+//   return spotifyApi.searchTracks(queryTerm, {limit: 5})
+//     .then(function(data) {
+//    console.log('data', data);
 
 
+//     }, function(err) {
+//       console.error('error',err);
+//     });
+// }
 
-// $.ajax({
-//    url: 'https://api.spotify.com/v1/users/126955706/playlists',
-//    headers: {
-//        'Authorization': 'Bearer ' + 'BQD-QeP6VqzjFcPy8xpHdmiPm3Wwtrxu9UotDZt3JBsP2cL5o_2CJEvJLuxFPB2yW4vvyE0AQAqcsbC4q1RAaw'
-//    },
-//    success: function(response) {
-//    	console.log('this is response', response)
-//           }
-// });
+var sonApi = 'https://api.spotify.com/v1/tracks/?ids=0wIEGEWdswpwOnpM3nuwkC,3b2J8pXcrlrGMY4q7zsBtI,3oFwFUxhOgem0hPpFsor1n';
+var rumbaApi = 'https://api.spotify.com/v1/tracks/?ids=4F03szV3H55QIiP94TXrsT,1KIimqBpVLFcS5Yc7DSQg9,47lSbhOlKE8fJJ2jc6HqRQ';
+var salsaApi = 'https://api.spotify.com/v1/tracks/?ids=3Pacy6CMa8HPNVfeA3wkPQ,3N3oba9mXWFD4NOnZFK46Q,0neyZWsvpfpyVutNtRjj8g,3zpPox6fuNguDVxRjSoBaJ,4rGvcJFVrCO22Rj9pCJ9EL';
+
+
+$.get(sonApi, function(response) {
+  console.log('this is son', response) 
+    $('.song-track span').text(response.tracks.name);
+    // $('#humidity span').text(response.main.humidity); 
+    // $('#wind span').text(response.wind.speed);
+    console.log('track name', name)
+  });
+
+
+
 
 
 $('#timbaBtn').on('click', function(event) {
