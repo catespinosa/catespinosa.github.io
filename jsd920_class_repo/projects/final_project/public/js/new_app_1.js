d60864969915501c3215a0717ca2d40e11308624
Espinosa <mzy36638@admins-MacBook-Pro.local>
2016-11-27 01:29:43 -0500

    cleareed modal when closing so tracks get removed

--- jsd920_class_repo/projects/final_project/public/js/new_app_1.js
+++ jsd920_class_repo/projects/final_project/public/js/new_app_1.js
@@ -1,122 +1,102 @@
 $(document).ready(function () {
 
-var spotifyApi = new SpotifyWebApi();
-// spotifyApi.setAccessToken('BQD-QeP6VqzjFcPy8xpHdmiPm3Wwtrxu9UotDZt3JBsP2cL5o_2CJEvJLuxFPB2yW4vvyE0AQAqcsbC4q1RAaw');
-// spotifyApi.getUserPlaylists()  // note that we don't pass a user id
-//   .then(function(data) {
-//     console.log('User playlists', data);
-//   }, function(err) {
-//     console.error(err);
-//   });
-// searchTrack('Llego la musica cubana')
+  var sonApi = 'https://api.spotify.com/v1/tracks/?ids=3oFwFUxhOgem0hPpFsor1n,0kOy6QY60Llpp3wnaDddfB,0wIEGEWdswpwOnpM3nuwkC,3b2J8pXcrlrGMY4q7zsBtI,1XLF6Mju0XeyCL0XrM3boG';
+  var rumbaApi = 'https://api.spotify.com/v1/tracks/?ids=4F03szV3H55QIiP94TXrsT,6NJZNTuXQjWiDCptV0ASze,1KIimqBpVLFcS5Yc7DSQg9,47lSbhOlKE8fJJ2jc6HqRQ';
+  var salsaApi = 'https://api.spotify.com/v1/tracks/?ids=3Pacy6CMa8HPNVfeA3wkPQ,3N3oba9mXWFD4NOnZFK46Q,0neyZWsvpfpyVutNtRjj8g,3zpPox6fuNguDVxRjSoBaJ,0ndv7r0b5KlDj3ZAmzpGQZ';
 
-// function searchTrack(queryTerm) {
-//   // store the current promise in case we need to abort it
-//   return spotifyApi.searchTracks(queryTerm, {limit: 5})
-//     .then(function(data) {
-//    console.log('data', data);
+  function parseResults(result){
+    var all_tracks = []; //this is a placeholder for what we want to return
+    //console.log('this is the tracks array',all_tracks)
+    result.forEach(function(row){
+      var onetrack = {
+        title : row.name,
+        artist : row.artists[0].name,
+        album : row.album.name,
+        link : row.preview_url,
+        image : row.album.images[2].url
+      };
+      all_tracks.push(onetrack); //we want to push the object in to the all tracks array
+    });
 
+    return all_tracks;
+  };
 
-//     }, function(err) {
-//       console.error('error',err);
-//     });
-// }
+  //template for displaying tracks in modal
+  function addTracksToPage(tracks) {
+      var source = $('#tracks-template').html();
+      var htmlbuilder = Handlebars.compile(source);
+      var tracksDataObj = {
+        rows: tracks
+      };
+      var htmlBlock = htmlbuilder(tracksDataObj);
 
-var sonApi = 'https://api.spotify.com/v1/tracks/?ids=0wIEGEWdswpwOnpM3nuwkC,3b2J8pXcrlrGMY4q7zsBtI,3oFwFUxhOgem0hPpFsor1n';
-var rumbaApi = 'https://api.spotify.com/v1/tracks/?ids=4F03szV3H55QIiP94TXrsT,1KIimqBpVLFcS5Yc7DSQg9,47lSbhOlKE8fJJ2jc6HqRQ';
-var salsaApi = 'https://api.spotify.com/v1/tracks/?ids=3Pacy6CMa8HPNVfeA3wkPQ,3N3oba9mXWFD4NOnZFK46Q,0neyZWsvpfpyVutNtRjj8g,3zpPox6fuNguDVxRjSoBaJ,4rGvcJFVrCO22Rj9pCJ9EL';
+      $('section#loadplaylists').append(htmlBlock);
+  };
 
-
-
-function parseResults(result){
-  var all_tracks = []; //this is a placeholder for what we want to return
-  console.log('this is the tracks array',all_tracks)
-  result.forEach(function(row){
-    var onetrack = {
-      title : row.name,
-      artist : row.artists[0].name,
-      album : row.album.name,
-      link : row.preview_url,
-      image : row.album.images[2].url
-    };
-    all_tracks.push(onetrack); //we want to push the object in to the all tracks array
-  });
-
-  return all_tracks;
-};
+//reset contents on modal so the tracks don't get added 
+//on top of the ones that were already there
+  $("#myModal").on("hidden.bs.modal", function(){
+    $("#loadplaylists").html("");
+});
 
 
-$('#timbaBtn').on('click', function(event) {
-  event.preventDefault(); 
-  //console.log('playlist 1', event)
+  $('#sonBtn').on('click', function(event) {
+    event.preventDefault(); 
+    //console.log('playlist 1', event)
 
-  $("#myModal").modal('show');
-  //start with the son cubano tracks
-  $.get(sonApi, function(response) {
-    console.log('this is son', response) 
+    $("#myModal").modal('show');
+    //start with the son cubano tracks
+    $.get(sonApi, function(response) {
+      //console.log('this is son', response) 
 
-    var results = response.tracks;
+      var results = response.tracks;
 
-    var music = parseResults(results);
-    //console.log(music);
-    addTracksToPage(music);
+      var music = parseResults(results);
+      //console.log(music);
+      addTracksToPage(music);    
 
-
-    function addTracksToPage(tracks) {
-    var source = $('#tracks-template').html();
-    var htmlbuilder = Handlebars.compile(source);
-    var tracksDataObj = {
-      rows: tracks
-    };
-    var htmlBlock = htmlbuilder(tracksDataObj);
-
-    $('section#loadplaylists').append(htmlBlock);
-    }
-
-
-
+    });
 
   });
 
-});
+  $('#timbaBtn').on('click', function(event) {
+    event.preventDefault(); 
+    //console.log('playlist 2', event)
 
-$('#sonBtn').on('click', function(event) {
-  event.preventDefault(); 
-  console.log('playlist 2', event)
-});
+    $("#myModal").modal('show');
+    $.get(salsaApi, function(response) {
+      // console.log('this is salsa', response) 
 
-$('#rumbaBtn').on('click', function(event) {
-  event.preventDefault(); 
-  console.log('playlist 3', event)
-});
+      var results = response.tracks;
 
+      var music = parseResults(results);
+      //console.log(music);
+      addTracksToPage(music);
 
-
-// var Spotify = require('spotify-web-api-js');
-// var s = new Spotify();
-
-
-// // https://api.spotify.com/v1/users/wizzler/playlists" -H "Authorization: Bearer {your access token}
+    });
 
 
-// // // set it in the wrapper
-// var spotifyApi = new SpotifyWebApi();
-// spotifyApi.setAccessToken('BQD-QeP6VqzjFcPy8xpHdmiPm3Wwtrxu9UotDZt3JBsP2cL5o_2CJEvJLuxFPB2yW4vvyE0AQAqcsbC4q1RAaw');
-// spotifyApi.getUserPlaylists('126955706')
-//   .then(function(data) {
-//     console.log('User playlists', data);
-//   }, function(err) {
-//     console.error(err);
-//   });
+  });
 
-// spotifyApi.getPlaylist('126955706', 'BQD-QeP6VqzjFcPy8xpHdmiPm3Wwtrxu9UotDZt3JBsP2cL5o_2CJEvJLuxFPB2yW4vvyE0AQAqcsbC4q1RAaw')
-//   .then(function(data) {
-//     console.log('User playlist', data);
-//   }, function(err) {
-//     console.error(err);
-//   });
+  $('#rumbaBtn').on('click', function(event) {
+    event.preventDefault(); 
+    // console.log('playlist 3', event)
+    $("#myModal").modal('show');
+    $.get(rumbaApi, function(response) {
+      //console.log('this is rumba', response); 
 
- });
+      var results = response.tracks;
+
+      var music = parseResults(results);
+      //console.log(music);
+      addTracksToPage(music);
+
+    });
+
+  });
+
+});//document ready
+
 
 
 
