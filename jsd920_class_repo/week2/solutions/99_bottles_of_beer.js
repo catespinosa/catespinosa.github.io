18421573bce10186324930cf2e4b6014266e291e
Espinosa <mzy36638@admins-MacBook-Pro.local>
2016-11-29 12:52:25 -0500

    updating missing homeworks, moved labs to homework folders

--- jsd920_class_repo/week2/solutions/99_bottles_of_beer.js
+++ jsd920_class_repo/week2/solutions/99_bottles_of_beer.js
@@ -1,11 +1,3 @@
-
-
-// 99 BOTTLES OF BEER
-// Write a script that prints the lyrics to "99 Bottles of Beer on the Wall" in the terminal
-// Make sure your program can handle both singular and plural cases (i.e. both "100 bottles of beer" and "1 bottle of beer")
-// Hint: you will want to use a for loop that starts at 99 and counts down to 0
-
-
 for (counter = 99; counter >= 1; counter -= 1) {
     console.log(counter)
     if (counter == 2) {
