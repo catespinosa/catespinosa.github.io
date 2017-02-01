fde7702acd8c4b0b5a4203714970fc86e1de1cdc
catespinosa <cat.espinosa@gmail.com>
2017-01-31 23:04:08 -0500

    made popup work, handlebars

--- jsd920_class_repo/projects/feedr/js/app.js
+++ jsd920_class_repo/projects/feedr/js/app.js
@@ -8,27 +8,7 @@
  	var articles = parseResults(results);
  	console.log(articles);
  	addArticlesToPage(articles);
- 	
- 	// make all article title links NOT go to the source but instead open the pop up
- 	$('section.articleContent a').on('click', function(event){
- 		event.preventDefault();
- 		var h3_text = $(this).children('h3')[0];
- 		var title = response.new.title;
- 		var link = response.new.link;
- 		// var content = response.new.content.plain;
- 		//showPopUp(title, link, content);
- 		//remove hidden class from pop up
- 		$('#popUp').removeClass('.loader hidden');
- 		$('.loader .container').removeAttr('display');
- 	});
-});
 
- function showPopUp(title, link, content) {
- 	// make pop up div visible by removing the "loader" and "hidden" classes
- 	// replace the html dom text in the pop up div with that of the article
- 	// add an onclick event to be able to close the pop up
- 	// make sure the read more from source link opens a new tab/window
- }
 
 function parseResults(results){
 	var all_articles = []; //this is a placeholder for what we want to return
@@ -47,7 +27,7 @@ function parseResults(results){
 	return all_articles;
 }
 
-function addArticlesToPage(articles) {
+ 	function addArticlesToPage(articles) {
 	var source = $('#articles-template').html();
 	var htmlbuilder = Handlebars.compile(source);
 	var articlesDataObj = {
@@ -56,4 +36,36 @@ function addArticlesToPage(articles) {
 	var htmlBlock = htmlbuilder(articlesDataObj);
 
 	$('section#main').append(htmlBlock);
+	
 }
+ 	
+ 	// make all article title links NOT go to the source but instead open the pop up
+ 	$('section.articleContent a').on('click', function(event){
+ 		event.preventDefault();
+ 		var h3_text = $(this).children('h3')[0];
+ 		var title = response.new.title;
+ 		var link = response.new.link;
+ 		var description = response.new.content;
+ 		showPopUp(title, link, description);
+ 		//remove hidden class from pop up
+ 	});
+});
+
+ function showPopUp(title, link, description) {
+ 	// make pop up div visible by removing the "loader" and "hidden" classes
+ 		$('#popUp').removeClass('loader hidden');
+ 		$('#popUp.loader .container').css('display','');
+ 	// replace the html dom text in the pop up div with that of the article
+ 		function addtexttopopup (response) {
+ 			var popUptext = response.articles;
+ 			var popup = parseResults(popUptext);
+ 			addArticlesToPage(popup);
+ 		};
+ 	// add an onclick event to be able to close the pop up
+ 	$('.closePopUp a').on('click', function (event) {
+ 		event.preventDefault();
+ 		$('#popUp').hide();
+ 	});
+ 	// make sure the read more from source link opens a new tab/window
+ };
+
