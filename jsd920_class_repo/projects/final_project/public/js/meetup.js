ce9e072bf613c93ab034233b2722bd65e4914bf0
catespinosa <cat.espinosa@gmail.com>
2017-03-04 00:17:25 -0500

    css changes

--- jsd920_class_repo/projects/final_project/public/js/meetup.js
+++ jsd920_class_repo/projects/final_project/public/js/meetup.js
@@ -37,30 +37,34 @@
     
     };
 
-          $(document).ready(function() {
+          
               
               var $results = $("#results");
 
               $results.html("<p>Finding meetups near you.</p>");
 
               fetchGroups("https://api.meetup.com/find/groups?upcoming_events=true&photo-host=public&page=20&fallback_suggestions=true&sig_id=217660822&topic_id=116988%2C468792%2C120760%2C59363&sig=a04fbd81a81befd09d1ec4d041e8b8831f3b87fd", function(res) {
-                  console.log("totally done");
+                        console.log("totally done");
 
-                  var s = "";
-                  for(var i=0;i<res.length; i++) {
-                      var group = res[i];
-                      s += "<h2>"+(i+1)+" <a href='"+group.link+"'>"+group.name+"</a></h2>";
-                      if(group.group_photo && group.group_photo.thumb_link) {
-                          s += "<img src=\"" + group.group_photo.thumb_link + "\" align=\"left\">";
-                      }
-                      s += "<p>Location: "+group.city + ", " + group.state + " " + group.country + "</p><br clear=\"left\">";
-                  }
-                  $results.html(s);
-                  
+                        var s = "";
+                        for(var i=0;i<res.length; i++) {
+                            var group = res[i];
+                            
+                            if(group.group_photo && group.group_photo.thumb_link) {
+                                s += "<img src=\"" + group.group_photo.thumb_link + "\">";
+                            }
+                            s += "<h3>"+" <a href='"+group.link+"'>"+group.name+"</a></h3>";
+                            s += "<p>Location: "+group.city + ", " + group.state + " " + group.country + "</p><br clear=\"left\">";
+                            s += "<p>"+group.description + "</p><br clear=\"left\">";
+                        }
+                        $results.html(s);
+
+
+
                   
               });
         
-});
+
 
 
 
